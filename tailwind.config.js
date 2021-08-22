c5f4646a232237a582ad22452fef61ee73a1b75a
Doug <dougneale@gmail.com>
2021-08-22 17:09:09 +1000

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    container: {
+      padding: {
+        DEFAULT: "2rem",
+        sm: "3rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+      center: true,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
